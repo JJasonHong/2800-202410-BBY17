98fd91b088f90affa1da507e3a2954eb6fa28ec0
Andre Hindarmara <hindarmara@gmail.com>
2024-05-15 17:22:20 -0700

    create landing page/index.ejs, footer.ejs, add favicon on index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const bcrypt = require("bcrypt");
 const saltRounds = 12;
-
+const path = require('path');
+const favicon = require('serve-favicon');
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -14,6 +15,10 @@ const app = express();
 const Joi = require("joi");
 
 const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+app.set("view engine", "ejs");
+
+
+
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
@@ -29,9 +34,9 @@ var { database } = include("databaseConnection");
 
 const userCollection = database.db(mongodb_database).collection("users");
 
-app.set("view engine", "ejs");
-
 const url = require('url');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -91,9 +96,14 @@ app.use((req, res, next) => {
   
   // app.use('/', sessionValidation);
   app.get("/", (req, res) => {
+    res.render("hero");
    
   });
+
   
+  
+  app.use(express.static(__dirname + "/public"));
+
   app.listen(port, () => {
     console.log("Node application listening on port" + port);
   });
